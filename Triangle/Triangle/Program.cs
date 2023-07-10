cdbf399edb04d6b86a1ae52ef7396addee193aab
Gustavo de Souza <noobshippuden13@gmail.com>
2023-07-10 15:35:49 -0300

    Feat: Added the triangle script

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 10; j > i; j--)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 0; k < 2; k++)
+                {
+                    for (int a = 0; a < i; a++)
+                    {
+                        Console.Write('.');
+                    }
+                }
+                Console.WriteLine(".");
+            }
+        }
+    }
+}
